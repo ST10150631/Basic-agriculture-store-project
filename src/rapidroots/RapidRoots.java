16f6021c9d9d5fab0853ba8eae15b4f9404f3bb1
Mike_Turner <st10150631@vcconnect.edu.za>
2023-01-19 14:46:06 +0200

    initial commit

---
+++ src/rapidroots/RapidRoots.java
@@ -0,0 +1,27 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rapidroots;
+
+/**
+ *
+ * @author Mike
+ */
+import javax.swing.*;
+public class RapidRoots {
+
+   
+    public static void main(String[] args) {
+        // TODO code application logic here
+        our3LPots();
+    }
+    static void our3LPots(){
+       double potENVolume = 3*1.2;
+    String   numberOfPots ; 
+      numberOfPots=JOptionPane.showInputDialog("How many pots would you like? ");
+      
+        System.out.print(potENVolume);
+    }
+    
+}
